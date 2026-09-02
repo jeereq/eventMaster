8aa4b0cd4788b3e275f413a3eea526e439ae522d
minganda <mingandajeereq@gmail.com>
2026-09-03 00:29:51 +0100

    feat: define constants for Twitter image properties including dynamic behavior, alt text, size, and content type; export default from opengraph-image module

--- frontend/src/app/twitter-image.tsx
+++ frontend/src/app/twitter-image.tsx
@@ -1,1 +1,5 @@
-export { default, alt, size, contentType, dynamic } from './opengraph-image';
+export const dynamic = 'force-dynamic';
+export const alt = 'EventMaster';
+export const size = { width: 1200, height: 630 };
+export const contentType = 'image/png';
+export { default } from './opengraph-image';
